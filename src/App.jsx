ad9688e746e0e149fd866629c9f119ce1d7582d1
vasconcellosflori-code <vasconcellos.flori@gmail.com>
2026-05-13 08:21:15 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Gelato Lab V02</h1>
+      <p>Aplicativo carregando corretamente.</p>
+    </div>
+  );
+}
+
 export default App;
